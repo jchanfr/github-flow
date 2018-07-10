eccf232e6fe0d57ad82ec445836a92807470dc15
jacques <jchanfr@gmail.com>
2018-07-10 15:15:45 +0200

    full

--- game.py
+++ game.py
@@ -18,7 +18,12 @@ class Game:
                 letters.remove(letter)
             else:
                 return False
-        return True
+        return self.__check_dictionary(word)
+
+    def __check_dictionary(self, word):
+        r = requests.get(f"https://wagon-dictionary.herokuapp.com/{word}")
+        response = r.json()
+        return response['found']
 
 
 
